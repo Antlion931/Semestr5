44f1d55c30bbf539a0be6c1086d6d5f2a8e53aab
Antlion931 <AlbertK931@protonmail.com>
2023-11-07 01:23:53 +0100

    JFTT List 2: First 3 exercises completed

---
+++ JFTT/Lista2/tests/test2.cpp
@@ -0,0 +1,36 @@
+/\
+*\
+Regular
+comment
+*\
+/
+The regular C comment number 1 has finished.
+
+/\
+\/ This is not a C++/C99 comment!
+
+This is followed by C++/C99 comment number 3.
+/\
+\
+\
+/ But this is a C++/C99 comment!
+The C++/C99 comment number 3 has finished.
+
+/\
+\* This is not a C or C++ comment!
+
+This is followed by regular C comment number 2.
+/\
+*/ This is a regular C comment *\
+but this is just a routine continuation *\
+and that was not the end either - but this is *\
+\
+/
+The regular C comment number 2 has finished.
+
+This is followed by regular C comment number 3.
+/\
+\
+\
+\
+* C comment */
